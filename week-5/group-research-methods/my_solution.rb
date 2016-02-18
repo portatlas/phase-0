ba0ad23efbfc8626b7c355c6a27e1df6a00ad820
portatlas <a.vathanaka@gmail.com>
2016-02-18 14:49:23 -0800

    Add my solution to 5.4

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -37,23 +37,26 @@ my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, 
 
 
 # Person 3
-# def my_array_sorting_method(source)
-#   sorted = source.sort!
-#   puts sorted.to_s
-# end
+def my_array_sorting_method(source)
+  p source.sort_by{|x| x.to_s}
+end
 
-# my_array_sorting_method(i_want_pets)
+my_array_sorting_method(i_want_pets)
 
-i_want_pets.sort do |a,b| a <=> b end
 
-# def my_hash_sorting_method(source)
+def my_hash_sorting_method(source)
+  p source.sort_by{|key,value| [value, key]}
+end
 
-# end
+my_hash_sorting_method(my_family_pets_ages)
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+# sort_by: is a method that sorts a collection of items against a block and returns an array of the sorted items. This method was useful because I was not able to use the sort method on elements in array that contains but strings and integers.
+
+# to_s: is a method that creates a string representation of the object that calls it. This method was useful in this situation because there were both strings and integers within the array. I was not able to use the sort method on its own because it wouldn't have been able to work with both strings and integers.  The solution was to use the method sort_by and convert each element in the array to a representation of a string.
+
+#Reading the ruby docs is still a bit difficult to grasp so many times I try to replicate the examples and change different mechanics of the code to see what it does. I also try to use ctrl + F to go to the keyword that makes most sense to search through the document.
+
 
 
 # # Person 4
